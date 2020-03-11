9d908743fde3c768482332bd408a6be4a26357f1
richa <jalan.richa@gmail.com>
2020-03-11 17:29:52 +0800

    workshop02

--- workshop02/local_main.js
+++ workshop02/local_main.js
@@ -18,15 +18,61 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mandatory workshop
 // TODO GET /api/states
-
-
+app.get('/api/states',
+	(req, resp) => { //handler
+		const result = db.findAllStates();
+		//status code 
+		resp.status(200)
+		//set content-Type 
+		resp.type('application/json')
+		resp.set('X-generated-on', (new Date()).toDateString())
+		resp.set('Access-control-Allow-Origin', '*')
+		resp.json(result)
+	}
+)
 // TODO GET /api/state/:state
-
+app.get('/api/state/:state',
+	(req, resp) => {//handler
+		const state = req.params.state;
+		const limit = parseInt(req.query.limit) || 10;
+		const offset = parseInt(req.querry.offset) || 0;
+		const result = db.findCitiesBystate(state, { offset: offset, limit: limit })
+		resp.status(200)
+		resp.type('application/json')
+		resp.json(result)
+	}
+)
 
 // TODO GET /api/city/:cityId
+app.get('/api/city/:city/cityId',
+	(req, resp) => {//handler
+		const city = req.params.cityId;
+		const result = db.findCityById(cityId)
+
+		resp.status(200)
+		resp.type('application/json')
+		resp.json(result)
+	}
+)
 
 
 // TODO POST /api/city
+app.post('/api/city',
+	(req, resp) => {
+		const body = req.body;
+		if (!db.validateForm(body)) {
+			resp.status(400)
+			resp.type('application/json')
+			resp.json({ 'message': 'incomplete form' })
+			return
+		}
+
+		db.insertCity(body)
+		resp.status(201)
+		resp.type('application/json')
+		resp.json({ message: 'created.' })
+	}
+)
 
 // Optional workshop
 // TODO HEAD /api/state/:state
@@ -35,9 +81,23 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // TODO GET /state/:state/count
-
-
+app.get('/api/state/:state/count',
+	(req, resp) => {//handler
+		const state = req.params.state;
+		const count = db.countCitiesInState(state)
+		const result = {
+			state: state,
+			numofcities: count,
+			timestamp: (new Date()).toDateString()
+		}
+		resp.status(200)
+		resp.type('application/json')
+		resp.json(result)
+	}
+)
 // TODO GET /api/city/:name
+
+
 
 
 // End of workshop
